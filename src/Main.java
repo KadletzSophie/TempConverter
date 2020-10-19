998a318b366eee12a41e5be0969d7938efbcaa39
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2020-10-19 18:51:43 +0200

    Javafx added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
 
     }
+
+    @Override
+    public void start(Stage stage){
+        VBox box_main = new VBox();
+        Scene scene = new Scene(box_main,500,500);
+        box_main.setBackground(new Background(new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        box_main.getChildren().addAll();
+        stage.setTitle("Temp. Converter");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
